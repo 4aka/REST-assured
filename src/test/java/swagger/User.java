9f9da630f4cfe6ff9b00243f27ca8cd4922fbf40
De Arch <30434449+4aka@users.noreply.github.com>
2021-05-23 23:22:52 +0300

    Update User.java

--- src/test/java/swagger/User.java
+++ src/test/java/swagger/User.java
@@ -11,17 +11,6 @@ public class User {
     private String phone;
     private int userStatus;
 
-//    public User(int id, String username, String firstName, String lastName, String email, String password, String phone, int userStatus) {
-//        this.id = id;
-//        this.username = username;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.email = email;
-//        this.password = password;
-//        this.phone = phone;
-//        this.userStatus = userStatus;
-//    }
-
     public int getId() {
         return id;
     }
